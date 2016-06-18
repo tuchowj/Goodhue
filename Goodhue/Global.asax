1d29de6e9f09033b5d0eef61f576fb5c1ed049c9
Jonah Tuchow <tuchowj@carleton.edu>
2016-06-18 14:45:30 -0500

    first commit

---
+++ Goodhue/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Goodhue.MvcApplication" Language="C#" %>
---
+++ Goodhue/Models/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Goodhue.Models
+{
+    public class Car
+    {
+        //unchanging
+        public int ID { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+        //TODO: mileage and date put in service?
+
+        //changing
+        public string location { get; set; }
+        public int Odometer { get; set; }
+        public int OilChangeMiles { get; set; }
+        public DateTime LastReservation { get; set; }
+        //TODO: comments?
+    }
+
+    public class CarDBContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+    }
+}
