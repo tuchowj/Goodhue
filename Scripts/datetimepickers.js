b27293d0962f7d11e65bb2bf87ae9363aa38e3ff
tuchowj <tuchowj@carleton.edu>
2016-08-13 15:29:56 -0500

    files added

---
+++ Scripts/datetimepickers.js
@@ -0,0 +1,3 @@
+﻿$(".date").bootstrapMaterialDatePicker({ format: 'MM/DD/YYYY', time: false, clearButton: true });
+$('#checkoutTime').bootstrapMaterialDatePicker({ format: 'MM/DD/YYYY hh:mm A', shortTime: true});
+$('#returnTime').bootstrapMaterialDatePicker({ format: 'MM/DD/YYYY hh:mm A', shortTime: true});
